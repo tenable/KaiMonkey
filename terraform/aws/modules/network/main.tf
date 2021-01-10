fa183a14b3d530f380819ae078b51216d82624b2
Iceal <iceal@accurics.com>
2021-01-10 00:46:29 -0800

    Update main.tf

--- terraform/aws/modules/network/main.tf
+++ terraform/aws/modules/network/main.tf
@@ -108,7 +108,7 @@ resource "aws_security_group" "km_alb_sg" {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
