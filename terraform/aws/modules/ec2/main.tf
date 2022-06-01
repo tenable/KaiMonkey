5c53793bb2eddf7aa0fa7b9820506f07a787f415
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-06-01 08:46:42 +0530

    Create main.tf

---
+++ terraform/aws/modules/ec2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+ #text
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
