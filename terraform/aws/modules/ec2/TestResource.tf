3c48fdf4c13090c7a81721dc63f4e09c1432d862
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-07-04 18:21:45 +0530

    Create TestResource.tf

---
+++ terraform/aws/modules/ec2/TestResource.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+ #text
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+  
+resource "aws_instance" "app_server11" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  monitoring = var.monitoring
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
