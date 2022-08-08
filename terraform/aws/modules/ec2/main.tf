62c6b577de4278a20a197859ec7770e65c212ce5
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-08-08 12:58:58 +0530

    Update main.tf

--- terraform/aws/modules/ec2/main.tf
+++ terraform/aws/modules/ec2/main.tf
@@ -23,3 +23,12 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_instance" "app_server123" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
