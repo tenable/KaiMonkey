b87df6871b93f12c29bdb3b4f41baeb528acb2bd
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2023-04-25 15:08:41 +0530

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.11"
+  cloud {
+        organization = "taconet"
+
+        workspaces {
+            tags = ["app:taco"]
+        }
     }
-  }
 }
 
 provider "aws" {
